bbee36353bc00d677a11e90cbedfa04183104f29
Rafael Mota Alves <rma7@cin.ufpe.br>
2017-06-19 08:44:11 -0300

    Update Passageiro.java

--- src/ClassesBasicas/Passageiro.java
+++ src/ClassesBasicas/Passageiro.java
@@ -2,7 +2,6 @@ package ClassesBasicas;
 
 public class Passageiro extends Pessoa {
 	private int milhas;
-	private Bagagem bagagem;
 	public Passageiro(String cpf,String nome){
 		super(cpf, nome);
 		this.milhas=0;
@@ -12,13 +11,5 @@ public class Passageiro extends Pessoa {
 	}
 	public void setMilhas(int milhas){
 		this.milhas=milhas;
-	}
-	public Bagagem getBagagem(){
-		return this.bagagem;
-	}
-	public void setBagagem(Bagagem bagagem){
-		this.bagagem=bagagem;
-	}
-	
-	
+	}		
 }
