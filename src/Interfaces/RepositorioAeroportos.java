349c1cbc1cdf9aaa9237c5fcac9d4c55e477456f
Lucas-CardosoO <lccao@cin.ufpe.br>
2017-06-21 13:59:17 -0300

    Updated RepositorioAeroporto

--- src/Interfaces/RepositorioAeroportos.java
+++ src/Interfaces/RepositorioAeroportos.java
@@ -3,9 +3,9 @@ import ClassesBasicas.Aeroporto;
 import Exceptions.AeroportoNotFoundException;
 
 public interface RepositorioAeroportos {
-	void inserir(String cidade, String codigo, int capacidade);
-	Aeroporto procurar(String cidade) throws AeroportoNotFoundException;
-	void atualizar(String codigo, int capacidade) throws AeroportoNotFoundException;
+	void inserir(Aeroporto aeroporto);
+	Aeroporto procurar(String codigo) throws AeroportoNotFoundException;
+	void atualizar(String codigo, Aeroporto aeroporto) throws AeroportoNotFoundException;
 	void remover(String codigo) throws AeroportoNotFoundException;	
 }
 
