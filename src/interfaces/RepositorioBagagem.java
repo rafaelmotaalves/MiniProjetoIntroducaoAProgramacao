640de660cdfa302ae649dc1ecf6bc35b888dc511
edenalsant <eeas@cin.ufpe.br>
2017-06-24 19:43:35 -0300

    Add files via upload
    small changes in Bagagem interface

--- src/interfaces/RepositorioBagagem.java
+++ src/interfaces/RepositorioBagagem.java
@@ -1,10 +1,11 @@
-package interfaces;
+
 import classesBasicas.Bagagem;
 import exceptions.bagagemJaExistenteException;
 import exceptions.bagagemNaoExisteException;
+import exceptions.bagagemPesoException;
 
 public interface RepositorioBagagem {
-	void inserir(Bagagem bagagem) throws bagagemJaExistenteException;
+	void inserir(Bagagem bagagem) throws bagagemJaExistenteException, bagagemPesoException;
 	Bagagem procurar(int cod) throws bagagemNaoExisteException;
 	void atualizar(int cod, Bagagem bagagem) throws bagagemNaoExisteException;
 	void remover(int cod) throws bagagemNaoExisteException;
