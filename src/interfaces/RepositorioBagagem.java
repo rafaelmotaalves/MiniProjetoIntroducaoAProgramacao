8db414ea38ca464fc410332cb38e7e9efc7a2831
edenalsant <eeas@cin.ufpe.br>
2017-06-25 19:17:40 -0300

    adição de método

--- src/interfaces/RepositorioBagagem.java
+++ src/interfaces/RepositorioBagagem.java
@@ -1,13 +1,15 @@
-package interfaces;
+
 import classesBasicas.Bagagem;
-import exceptions.bagagemJaExistenteException;
-import exceptions.bagagemNaoExisteException;
-import exceptions.bagagemPesoException;
+import classesBasicas.Voo;
+import exceptions.BagagemJaExistenteException;
+import exceptions.BagagemNaoExisteException;
+import exceptions.BagagemPesoException;
 
 public interface RepositorioBagagem {
-	void inserir(Bagagem bagagem) throws bagagemJaExistenteException, bagagemPesoException;
-	Bagagem procurar(int cod) throws bagagemNaoExisteException;
-	void atualizar(int cod, Bagagem bagagem) throws bagagemNaoExisteException;
-	void remover(int cod) throws bagagemNaoExisteException;
+	void inserir(Bagagem bagagem) throws BagagemJaExistenteException, BagagemPesoException;
+	Bagagem procurar(int cod) throws BagagemNaoExisteException;
+	void atualizar(int cod, Bagagem bagagem) throws BagagemNaoExisteException;
+	void remover(int cod) throws BagagemNaoExisteException;
 	boolean existe (int Cod);
+	void remover(Voo voo) throws BagagemNaoExisteException;
 }
