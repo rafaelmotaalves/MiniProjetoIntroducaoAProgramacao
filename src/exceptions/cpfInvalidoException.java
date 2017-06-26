a4b786684a2fb3c3c5f077ccffa1e7af60ea5e25
Rafael Mota Alves <rma7@cin.ufpe.br>
2017-06-26 09:03:32 -0300

    Delete cpfInvalidoException.java

--- src/exceptions/cpfInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class cpfInvalidoException extends Exception {
-	public cpfInvalidoException(){
-		super("O cpf informado não possui o numero correto de caracteres");
-	}
-}
