c7591d180e677d37622b482dbce7d217b067456a
Rafael Mota Alves <rma7@cin.ufpe.br>
2017-06-26 09:03:47 -0300

    Delete cpfNaoCadastradoException.java

--- src/exceptions/cpfNaoCadastradoException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class cpfNaoCadastradoException extends Exception {
-	public cpfNaoCadastradoException(){
-		super("Não há nenhuma pessoa cadastrada com este CPF");
-	}
-}
