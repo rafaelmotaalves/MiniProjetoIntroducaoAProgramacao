c48e8e691817febf977c09c4f9ddcdb486f9f529
victorbrayner <victorbrayner@gmail.com>
2017-06-26 08:38:07 -0300

    comentários

--- src/repositorios/RepositorioAviaoArray.java
+++ src/repositorios/RepositorioAviaoArray.java
@@ -13,13 +13,14 @@ public class RepositorioAviaoArray implements RepositorioAviao {
 		this.aviao=new Aviao[tamanhoArray];
 		this.indice=0;
 	}
-	public boolean existe(int ID){
+	//checa de existe aviao no repositorio
+	public boolean existe(int id){
 		if(estaCheio()){
 			duplicarTamanho();	
 		}
 		boolean existe = false;
 		for(int c=0; c<this.indice; c++){
-			if(aviao[c].getID()==ID){
+			if(aviao[c].getID()==id){
 				existe=true;
 			}
 		}
@@ -28,6 +29,8 @@ public class RepositorioAviaoArray implements RepositorioAviao {
 	public int getTamanho(){
 		return aviao.length;
 	}
+	
+	//dupĺica o tamanho do array
 	public void duplicarTamanho(){
 		Aviao[] aux = new Aviao[tamanhoArray*2];
 		for(int i=0; i<this.tamanhoArray ;i++){
@@ -36,9 +39,11 @@ public class RepositorioAviaoArray implements RepositorioAviao {
 		this.aviao=aux;
 		this.tamanhoArray=this.tamanhoArray*2;
 	}
+	//checa se o array está lotado
 	public boolean estaCheio(){
 		return this.indice==this.tamanhoArray;
 	}
+	//insere aviao
 	public void inserir(Aviao aviao) throws AviaoJaCadastradoException {
 		if(existe(aviao.getID())){
 			throw new AviaoJaCadastradoException();
@@ -48,42 +53,46 @@ public class RepositorioAviaoArray implements RepositorioAviao {
 			indice++;
 		}
 	}
-	public Aviao procurar(int ID) throws IdNaoCadastradaException{
+	//procura aviao no repositorio
+	public Aviao procurar(int id) throws IdNaoCadastradaException{
 		boolean encontrado=false;
 		Aviao procurado=null;
-		if(existe(ID)){
+		if(existe(id)){
 			for(int c=0; c<this.indice && !encontrado; c++){
-				if(this.aviao[c].getID()==ID){
+				if(this.aviao[c].getID()==id){
 					encontrado=true;
 					procurado=aviao[c];
 				}
 			}
 		}
 		else{
-			throw new IdNaoCadastradaException(ID);
+			throw new IdNaoCadastradaException(id);
 		}
 		return procurado;
 		
 	}
-	public void atualizar(int ID, Aviao aviao) throws IdNaoCadastradaException{
-		if(existe(ID)){
+	//atualiza aviao no repositorio substituindo por um novo objeto
+	public void atualizar(int id, Aviao aviao) throws IdNaoCadastradaException{
+		if(existe(id)){
 			boolean atualizado=false;
 			for(int c=0; c<this.indice && !atualizado; c++){
-				if(this.aviao[c].getID()==ID){
+				if(this.aviao[c].getID()==id){
 					this.aviao[c]=aviao;
 					atualizado=true;
 				}
 			}
 		}
 		else{
-			throw new IdNaoCadastradaException(ID);
+			throw new IdNaoCadastradaException(id);
 		}
 	}
-	public void remover(int ID) throws IdNaoCadastradaException{
+	
+	//remove aviao do repositorio 
+	public void remover(int id) throws IdNaoCadastradaException{
 		boolean removido=false;
-		if(existe(ID)){
+		if(existe(id)){
 			for(int c=0; c<indice && !removido; c++){
-				if(aviao[c].getID()==ID){
+				if(aviao[c].getID()==id){
 					this.aviao[c]=this.aviao[indice];
 					indice--;
 					removido=true;
@@ -91,7 +100,7 @@ public class RepositorioAviaoArray implements RepositorioAviao {
 			}
 		}
 		else{
-			throw new IdNaoCadastradaException(ID);
+			throw new IdNaoCadastradaException(id);
 		}
 	}
 	
